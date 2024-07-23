209cd8afb8b2ac62b8ff8298471dcf2654f040c6
joaonunez <joaovaldiglesias@gmail.com>
2024-07-23 01:54:32 -0400

    avanzando componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import React from "react";
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router'
+import { ContactList } from "./components/ContactList";
 function App() {
   return (
-    <div className="App">
-      hola
-    </div>
+    <Router>
+      <ContactList></ContactList>
+      <Routes>
+        <Route path="/" element></Route>
+      </Routes>
+    </Router>
   );
 }
 
