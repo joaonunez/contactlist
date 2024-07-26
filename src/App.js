6bcebafc3290ae0cfb212f909df4ab73e3c1bf1c
joaonunez <joaovaldiglesias@gmail.com>
2024-07-26 19:20:07 -0400

    stors creado

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import React,{createContext, useState} from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import { ContactList } from "./views/ContactList";
 import { AddContact }  from "./views/AddContact";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <>
     <Router>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Contact List</Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item col-6 col-xxl-12">
-                
-                <Link  className="nav-link active btn btn-info" to={"/AddContact"} >Añadir Nuevo Contacto</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<ContactList/>} ></Route>
         <Route path="/AddContact" element={<AddContact/>} ></Route>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Contact List
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item col-6 col-xxl-12">
+              <Link className="nav-link active btn btn-info" to={"/AddContact"}>
+                Añadir Nuevo Contacto
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
