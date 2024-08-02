757eb07f5479c776bb44a2da7035d7e4ed9c1288
joaonunez <joaovaldiglesias@gmail.com>
2024-08-02 12:20:40 -0400

    handleChangue event listo

--- src/views/EditContact.jsx
+++ src/views/EditContact.jsx
@@ -19,7 +19,19 @@ const EditContact = () =>{
             setContact(contactToEdit);//establece los campos con los valores del objeto contacto/mesero
         }
     },[id, store.meseros]); //es requerido usar las dependencias con las qu se interactuan para obtener el contacto correcto en base a la url
-    //se implementa 'store.meseros' para obtener todos los meseros en caso de que se actualicen los meseros desde algun otro lugar desde la aplicacion.
+    //se implementa 'store.meseros' para obtener todos los meseros en caso de que se actualicen los meseros desde algun otro lugar desde la aplicacion
+
+
+    //actualiza el estado contact con los nuevos valores que se ingresan en el formulario
+    const handleChange = (event) =>{
+        //name es el  atributo name del campo del formulario
+        //value es el valor actual del campo del formulario
+        const {name, value} = event.target; //event contiene informacion sobre el campo qie cambió y su valor, 'event.target' es el elemento  que disparo el evento 
+        setContact({
+            ...contact, //spread operator 
+            [name]: value
+        });
+    };
 }
 
 export default EditContact;
