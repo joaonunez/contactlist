1cceca9ddbabb7891ada0c877ae3b94751ac6af1
joaonunez <joaovaldiglesias@gmail.com>
2024-08-01 23:58:51 -0400

    state implementado

--- src/views/EditContact.jsx
+++ src/views/EditContact.jsx
@@ -3,7 +3,14 @@ import { useParams, useNavigate } from "react-router-dom";
 import { Context } from "../store/Context"
 const EditContact = () =>{
     const {actions, store } = useContext(Context)
-    const {id} = useParams
+    const {id} = useParams(); //con esta variable se obtiene el id del contacto apart de la url
+    const navigate = useNavigate();
+    const [contact, setContact] = useState({
+        name:"",
+        email:"",
+        phone:"",
+        address:""
+    });
 }
 
 export default EditContact;
