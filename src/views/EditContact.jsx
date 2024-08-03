35f12e63e7f2e265ad1fef56d1425d9cd78f5cdc
joaonunez <joaovaldiglesias@gmail.com>
2024-08-03 18:05:14 -0400

    handleChangue event listo

--- src/views/EditContact.jsx
+++ src/views/EditContact.jsx
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect, useState,} from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Context } from "../store/Context"
+
 const EditContact = () =>{
     const {actions, store } = useContext(Context)
     const {id} = useParams(); //con esta variable se obtiene el id del contacto apart de la url
@@ -32,6 +33,69 @@ const EditContact = () =>{
             [name]: value
         });
     };
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); //evita que se recargue la pagina
+        actions.updateContact(contact.id, contact); // cuando se dispara el evento submit, se guarda el contacto con los datos de los formularios
+        alert("Contacto Modificado")
+        navigate("/"); //navega al index de la aplicacion
+    };
+    return (
+        <div className="container mt-5 col-xxl-3">
+          <h2 className="text-center">Editar Contacto</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Nombre Completo</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Ingresa El Nombre"
+                name="name"
+                value={contact.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Ingresa Correo"
+                name="email"
+                value={contact.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Telefono</label>
+              <input
+                type="text"
+                className="form-control"
+                id="phone"
+                placeholder="Ingresa Teléfono"
+                name="phone"
+                value={contact.phone}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="address">Direccion</label>
+              <input
+                type="text"
+                className="form-control"
+                id="address"
+                placeholder="Ingresa Direccion"
+                name="address"
+                value={contact.address}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary btn-block mt-3">Guardar Cambios</button>
+          </form>
+        </div>
+      );
 }
 
 export default EditContact;
